1f15b57ad48c16e30b06cf995cb4aac68e5b9216
Yuleo <1173012900@qq.com>
2023-10-02 21:38:28 +0800

    update

--- api/login.js
+++ api/login.js
@@ -4,12 +4,14 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-module.exports = app.post('/input_ok', (req, res) => {
-  console.log('email: ',email);
-  console.log('password: ',password);
+app.post('/input_ok', (req, res) => {
+  console.log('email: ',req.body.email);
+  console.log('password: ',req.body.password);
   if (req.body.email === '1173012900@qq.com' && req.body.password === 'woshishuaige666') {
     res.redirect('https://pan.helloylh.com'); 
   } else {
     res.redirect('/404');
   }
 });
+
+module.exports = app; // 导出整个 Express 应用程序对象
