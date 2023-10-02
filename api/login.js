560a258a45a98aed2e79e45863e95dee1dbbdd7b
Yuleo <1173012900@qq.com>
2023-10-02 21:35:15 +0800

    update

--- api/login.js
+++ api/login.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/login', (req, res) => {
+app.post('/input_ok', (req, res) => {
   console.log('email: ',email);
   console.log('password: ',password);
   if (req.body.email === '1173012900@qq.com' && req.body.password === 'woshishuaige666') {
