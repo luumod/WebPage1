f4f19bbcc35ab4060fbdd7bc90bcfabccb3cb66a
Yuleo <1173012900@qq.com>
2023-10-02 21:06:17 +0800

    update

--- api/login.js
+++ api/login.js
@@ -2,9 +2,10 @@
 
 module.exports = (req, res) => {
     if (req.body.email === '1173012900@qq.com' && req.body.password === 'woshishuaige666') {
-      res.redirect('https://pan.helloylh.com'); // 登录成功，重定向到目标页面
+      
     } else {
-      res.redirect('/404'); // 登录失败，重定向到404页面或其他错误页面
+      res.redirect('https://pan.helloylh.com'); // 登录成功，重定向到目标页面
+      //res.redirect('/404'); // 登录失败，重定向到404页面或其他错误页面
     }
 };
   
