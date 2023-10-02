fa932818131f800283c5fef0d9e95e68a3677246
Yuleo <1173012900@qq.com>
2023-10-02 21:32:44 +0800

    update

--- api/login.js
+++ api/login.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-module.exports = app.post('/login', (req, res) => {
+app.post('/login', (req, res) => {
+  console.log('email: ',email);
+  console.log('password: ',password);
   if (req.body.email === '1173012900@qq.com' && req.body.password === 'woshishuaige666') {
     res.redirect('https://pan.helloylh.com'); 
   } else {
     res.redirect('/404');
   }
-});
-
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
 });
